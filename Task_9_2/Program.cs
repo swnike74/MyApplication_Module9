1c6e09b22df2d54afd74e694c50e037a0a44557e
Ведерников Сергей <swnike@mail.ru>
2024-12-26 23:51:43 +0500

    Update5

--- Task_9_2/Program.cs
+++ Task_9_2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Task_9_2
 {
@@ -11,32 +13,7 @@ namespace Task_9_2
      /// Дополнительно реализуйте проверку введённых данных от пользователя через конструкцию TryCatchFinally
      /// с использованием собственного типа исключения.
      /// </summary>
-
-        
-        public class NumberReader
-        {
-            public delegate void NumberEnteredDelegate(int num);
-            public event NumberEnteredDelegate? NumberEnteredEvent;
-            
-            void Read()
-            {
-                Console.WriteLine();
-                Console.WriteLine("Enter digit 1 for Forward sorting or 2 for backward sorting");
-                int number = Convert.ToInt32(Console.ReadLine());
-                if(number != 1 && number != 2) throw new FormatException();
-
-            }
-
-            NumberEntered(number);
-            protected virtual void NumberEntered(int num)
-            {
-                NumberEnteredEvent?.Invoke(num);
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            string[] ListofPerson =
+        static string[] ListofPerson =
             {
                 "Иванов",
                 "Петров",
@@ -44,20 +21,50 @@ namespace Task_9_2
                 "Сидоров",
                 "Естафьев"
             };
+        static void Main(string[] args)
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumberEnteredEvent += MakeDecision;
 
-            NumberReader nr = new NumberReader();
-            try
+            while (true)
             {
-
+                try
+                {
+                    numberReader.Read();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введено неверное значение");
+                }
             }
-            catch(FormatException)
-            {
-                Console.WriteLine("Введено неверное значение");
-            }
-           
 
         }
 
+        
+
+        static void MakeDecision(int num)
+        {
+            switch(num)
+            {
+                case 1:
+                    sort_forward(ListofPerson);
+                    foreach (string s in ListofPerson)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    break;
+                case 2:
+                    sort_backward(ListofPerson);
+                    foreach (string s in ListofPerson)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    break;
+
+            }
+        }
+
+       
         public static void sort_forward(string[] ListofPerson)
         {
             string stemp = "";
@@ -96,6 +103,27 @@ namespace Task_9_2
                     }
                 }
             }
+        }
+    }
+
+    public class NumberReader
+    {
+        public delegate void NumberEnteredDelegate(int num);
+        public event NumberEnteredDelegate? NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Enter digit 1 for Forward sorting or 2 for backward sorting");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2) throw new FormatException();
+
+            NumberEntered(number);
+        }
+        protected virtual void NumberEntered(int num)
+        {
+            NumberEnteredEvent?.Invoke(num);
         }
     }
 }
